fef2d3b9a68730a6a87117eb16406c17691bc7ca
SadiaAfrinLubna <sadialubna249@gmail.com>
2022-11-15 12:50:10 +0600

    init

---
+++ application/views/template/header.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>PLIL</title>
+        <link rel="stylesheet" href="https://bootswatch.com/3/darkly/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="<?php echo base_url(); ?>">PLIL</a>
+        </div>
+        <div id="navbar">
+          <ul class="nav navbar-nav">
+            <li><a href="<?php echo base_url(); ?>">Home</a></li>
+            <li><a href="<?php echo base_url(); ?>about">About</a></li>
+            <li><a href="<?php echo base_url(); ?>contact">Contact Us</a></li>
+            <li><a href="<?php echo base_url(); ?>login">Login</a></li>
+            <li><a href="<?php echo base_url(); ?>sign_up">Sign Up</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    
